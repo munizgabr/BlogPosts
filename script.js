5027299cd50539360973c0c44b56e99994761bb7
munizgabr <gabrielrrtolkien@gmail.com>
2022-06-07 21:42:36 -0300

    Editing the html and js

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function draw_title(text) {
 function draw_body(text) {
     const body = document.createElement("div");
     body.innerText = text;
+    body.style.background = "grey"
     return body;
 }
 
@@ -18,9 +19,41 @@ function draw_hr(){
     return hr;
 }
 
+
+
+
+/*
+function button_edit(){
+    const btn = document.createElement("button");
+    btn.innerHTML = "Editar";
+    btn.type = "submit"
+    btn.id = "edit-button"
+    return btn;
+}
+
+function button_send(){
+    const btn2 = document.createElement("button");
+    btn2.innerHTML = "Enviar";
+    btn2.type = "submit"
+    btn2.id = "end-editing"
+    return btn;
+}
+
+edit_button.addEventListener("click", function(){
+    paragraph.contentEditable = "true";
+});
+
+end_editing.addEventListener("click", function(){
+    paragraph.contentEditable = "false";
+})
+*/
+
 function draw_post(post) {
+
     document.getElementById('main').appendChild(draw_title(post.title));
     document.getElementById('main').appendChild(draw_body(post.body));
+    //document.getElementById('main').appendChild(button_edit());
+    //document.getElementById('main').appendChild(button_send());
     document.getElementById('main').appendChild(draw_hr());
 }
 
